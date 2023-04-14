64dcf6449acf12ed06a151e831f82dea3934be1e
João Henrique <joaohnsousa@gmail.com>
2023-04-14 14:02:18 -0300

    Correções Finalizadas

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
+import { Text, View, TextInput, Button, Image } from "react-native";
+import styles from "./Style/Style";
 
 export default function App() {
-  const [gasolina, setGasolina] = useState();
-  const [alcool, setAlcool] = useState();
-  const [gasolVant, setGasolVant] = useState(false);
-  const [alcoolVant, setAlcoolVant] = useState(false);
+  const [gasolina, setGasolina] = useState("");
+  const [Etanol, setEtanol] = useState("");
   const [text, setText] = useState("");
 
   function calcularEconomia() {
-    if (gasolina < alcool) {
-      setGasolVant(true);
-      setAlcoolVant(false);
-      setText("Gasolina está mais em conta!");
+    const gasolFloat = parseFloat(gasolina);
+    const etanolFloat = parseFloat(Etanol);
+    if (!isNaN(gasolFloat) && !isNaN(etanolFloat)) {
+      const combustVant = etanolFloat <= gasolFloat * 0.7;
+      setText(
+        combustVant ? "Etanol é mais vantajoso!" : "Gasolina é mais vantajosa!"
+      );
     } else {
-      setAlcoolVant(true);
-      setGasolVant(false);
-      setText("Álcool está mais em conta!");
+      setText("Valores inválidos");
     }
   }
   return (
     <View style={styles.container}>
       <Text style={styles.textTop}>Economize Mais!</Text>
+
       <Text style={styles.text}>Digite os valores dos combustiveis</Text>
       <StatusBar style="auto" />
       <TextInput
@@ -33,78 +34,50 @@ export default function App() {
         value={gasolina}
       />
       <TextInput
-        placeholder="Digite o valor do álcool"
+        placeholder="Digite o valor do Etanol"
         style={styles.input}
         keyboardType="numeric"
-        onChangeText={setAlcool}
-        value={alcool}
+        onChangeText={setEtanol}
+        value={Etanol}
       />
       <Button
         onPress={() => {
           calcularEconomia();
-          setAlcool("");
+          setEtanol("");
           setGasolina("");
-        
         }}
         title="Calcule"
         color="#28a745"
         accessibilityLabel="Learn more about this purple button"
       />
-      <Text style={styles.text}>{text}</Text>
-
+      <Text style={styles.textResult}>{text}</Text>
       <View>
-        {gasolVant || alcoolVant != false ?
-          gasolVant?(
+        {text ? (
+          <View style={styles.sectionImg}>
             <Image
               style={styles.image}
-              source={require("./images/gasolina.png")}
+              source={
+                text.includes("Gasolina")
+                  ? require("./images/gasolina.png")
+                  : text.includes("Etanol")
+                  ? require("./images/alcool.png")
+                  : null
+              }
             />
-          ) :  
-            <Image style={styles.image} source={require("./images/alcool.png")} />
-          :null
-        }
+            {text.includes("Etanol") ? (
+              <Text style={styles.sectionImg}>
+                OBS: O uso do etanol é somente é vantajoso pois o litro custar
+                até 70% do valor do litro da gasolina.
+              </Text>
+            ) : text.includes("Gasolina") ? (
+              <Text style={styles.sectionImg}>
+                OBS: O uso do etanol não é vantajoso pois o litro custar mais
+                que 70% do valor do litro da gasolina.
+              </Text>
+            ) : null}
+          </View>
+        ) : null}
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#17a2b8",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-    
-  },
-  input: {
-    backgroundColor: "#f8f9fa",
-    textAlign: "center",
-    height: 40,
-    width: 250,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 10,
-  },
-  image: {
-    marginTop: 15,
-    width: 200,
-    height: 200,
-  },
-  textTop: {
-    marginBottom: 20,
-    fontSize: 40,
-    backgroundColor: "#28a745",
-    borderRadius: 15,
-    padding: 20,
-    color: "#f8f9fa"
-  },
-  text: {
-    fontSize: 17,
-    color: "#f8f9fa"
-  },
-  textResult: {
-    marginTop: 20,
-  }
-});
---
+++ Style/Style.js
@@ -0,0 +1,49 @@
+import { StyleSheet} from "react-native";
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#17a2b8",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 10,
+      
+    },
+    sectionImg:{
+      alignItems: "center",
+      color: "#FFFF",
+      fontSize: 15,
+      marginTop: 5,
+    },
+    input: {
+      backgroundColor: "#f8f9fa",
+      textAlign: "center",
+      height: 40,
+      width: 250,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 10,
+    },
+    image: {
+      marginTop: 15,
+      width: 200,
+      height: 200,
+    },
+    textTop: {
+      marginBottom: 20,
+      fontSize: 40,
+      backgroundColor: "#28a745",
+      borderRadius: 15,
+      padding: 20,
+      color: "#f8f9fa"
+    },
+    text: {
+      fontSize: 17,
+      color: "#f8f9fa",
+    },
+    textResult: {
+      marginTop: 20,
+      fontSize: 17,
+      color: "#f8f9fa",
+    },
+  });
